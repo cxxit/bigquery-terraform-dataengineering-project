e12dd0f90482b71cbea2a6e7c6302151ce7a019d
Chin Xu Xin <xchi0019@student.monash.edu>
2026-07-08 05:55:21 +0800

    refactor terraform variables configuration file to not include function call file()

--- variables.tf
+++ variables.tf
@@ -1,35 +1,35 @@
 variable "credentials" {
-    description = "My Credentials"
-    default = file("./keys/my-creds.json")
+  description = "My Credentials"
+  default     = "./keys/my-creds.json"
 }
 
 variable "project" {
-    description = "GCP Project"
-    default = "terraform-demo-501718"
+  description = "GCP Project"
+  default     = "terraform-demo-501718"
 }
 
 variable "region" {
-    description = "GCP Project Region"
-    default = "asia-southeast1"
+  description = "GCP Project Region"
+  default     = "asia-southeast1"
 }
 
 variable "location" {
-    description = "GCP Project Location"
-    default = "asia-southeast1"
+  description = "GCP Project Location"
+  default     = "asia-southeast1"
 }
 
 variable "bq_dataset_name" {
-    description = "My BigQuery Dataset Name"
-    default = "demo_dataset"
+  description = "My BigQuery Dataset Name"
+  default     = "demo_dataset"
 }
 
 variable "gcp_bucket_name" {
-    description = "My Storage Bucket Name"
-    default = "terraform-demo-501718-terra-bucket"
+  description = "My Storage Bucket Name"
+  default     = "terraform-demo-501718-terra-bucket"
 }
 
 variable "gcp_storage_class" {
-    description = "Bucket Storage Class"
-    default = "STANDARD"
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
 }
 
